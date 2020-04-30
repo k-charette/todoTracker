bb68bade3e1cc5dd1d3c66388f5950e7e169fadf
Ken Charette <charettekenneth@gmail.com>
2020-04-30 02:13:43 -0400

    Login and signup for users. Shows up in database

--- functions/index.js
+++ functions/index.js
@@ -1,14 +1,16 @@
 const functions = require('firebase-functions');
 const app = require('express')();
 
-
 // All API calls will be done via the getAllTodos function
 const { getAllTodos, postOneTodo, deleteTodo, editTodo } = require('./APIs/todos')
+const { loginUser, signUpUser } = require('./APIs/users')
 
 // Assigning getAllTodos function to the /todos route 
 app.get('/todos', getAllTodos);
 app.post('/todo', postOneTodo);
 app.delete('/todo/:todoId', deleteTodo);
 app.put('/todo/:todoId', editTodo);
+app.post('/login', loginUser)
+app.post('/signup', signUpUser)
 
 exports.api = functions.https.onRequest(app);
---
+++ functions/util/config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    apiKey: "AIzaSyArJic-u8Hq_xdp-LKmU9W-4qaOPfixtoU",
+    authDomain: "todoapp-89149.firebaseapp.com",
+    databaseURL: "https://todoapp-89149.firebaseio.com",
+    projectId: "todoapp-89149",
+    storageBucket: "todoapp-89149.appspot.com",
+    messagingSenderId: "942533105231",
+    appId: "1:942533105231:web:777d05c27045d8c74e7c3a",
+    measurementId: "G-N331G40N1W"
+  };
+
