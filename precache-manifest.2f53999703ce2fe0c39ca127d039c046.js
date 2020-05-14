124789ccbfc43f6b1280c3d9d63bed06fb7205b2
Ken Charette <charettekenneth@gmail.com>
2020-05-14 13:32:48 -0400

    Updates

---
+++ precache-manifest.2f53999703ce2fe0c39ca127d039c046.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f1875864990dbe95395083baa212f2f",
+    "url": "/todoTracker/index.html"
+  },
+  {
+    "revision": "7f91cea130981a7245a0",
+    "url": "/todoTracker/static/css/main.9bdc3baf.chunk.css"
+  },
+  {
+    "revision": "0db1f600dd2d8d320272",
+    "url": "/todoTracker/static/js/2.7a23e137.chunk.js"
+  },
+  {
+    "revision": "68ba00d5eb083746d913686923a3d904",
+    "url": "/todoTracker/static/js/2.7a23e137.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7f91cea130981a7245a0",
+    "url": "/todoTracker/static/js/main.0e1708fd.chunk.js"
+  },
+  {
+    "revision": "053c409b97d2ce2e503f",
+    "url": "/todoTracker/static/js/runtime-main.f17d53aa.js"
+  }
+]);
